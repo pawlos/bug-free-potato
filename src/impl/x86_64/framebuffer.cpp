ba359eed86386ea94c8734df674761a1383f58bf
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2021-07-06 21:40:18 +0200

    MOUSE: Bigger pointer for mouse

--- src/impl/x86_64/framebuffer.cpp
+++ src/impl/x86_64/framebuffer.cpp
@@ -23,15 +23,23 @@ void Framebuffer::Draw(const uint8_t* what,
 	}
 }
 
-static uint64_t prevPixel = 0x000000;
+constexpr int cursor_size = 9;
+static uint64_t prevPixel[cursor_size] = {0x000000};
 void Framebuffer::DrawCursor(uint32_t x_pos, uint32_t y_pos)
 {
 	uintptr_t fb_addr   = this->m_addr;
 	uint32_t  fb_width  = this->m_width;
 	uint32_t  fb_stride = this->m_stride;
 	uint8_t   fb_bytes  = this->m_bpp / 8;
-	prevPixel = *(uint64_t *)(fb_addr + x_pos*fb_bytes + y_pos*fb_stride);
-	*(uint64_t *)(fb_addr + x_pos*fb_bytes + y_pos*fb_stride) = 0xff0000;
+	for (uint8_t i = 0; i < 3; i++)
+	{
+		for (uint8_t j = 0; j < 3; j++)
+		{
+			int pos = i*3+j;
+			prevPixel[pos] = *(uint64_t *)(fb_addr + (x_pos+j)*fb_bytes + (y_pos+i)*fb_stride);
+			*(uint64_t *)(fb_addr + (x_pos+j)*fb_bytes + (y_pos+i)*fb_stride) = 0xff0000;
+		}
+	}
 }
 
 void Framebuffer::EraseCursor(uint32_t x_pos, uint32_t y_pos)
@@ -40,7 +48,14 @@ void Framebuffer::EraseCursor(uint32_t x_pos, uint32_t y_pos)
 	uint32_t  fb_width  = this->m_width;
 	uint32_t  fb_stride = this->m_stride;
 	uint8_t   fb_bytes  = this->m_bpp / 8;
-	*(uint64_t *)(fb_addr + x_pos*fb_bytes + y_pos*fb_stride) = prevPixel;
+	for (uint8_t i = 0; i < 3; i++)
+	{
+		for (uint8_t j = 0; j < 3; j++)
+		{
+			int pos = i*3+j;
+			*(uint64_t *)(fb_addr + (x_pos+j)*fb_bytes + (y_pos+i)*fb_stride) = prevPixel[pos];
+		}
+	}
 }
 
 void Framebuffer::Clear(uint8_t r, uint8_t g, uint8_t b)
