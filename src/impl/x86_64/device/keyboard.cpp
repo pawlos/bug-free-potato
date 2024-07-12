06cdc93d8b74798035be9f84e5762d139630f42d
Paweł Łukasik <lukasik.pawel@gmail.com>
2024-07-12 20:18:31 +0200

    Use constrexpr

--- src/impl/x86_64/device/keyboard.cpp
+++ src/impl/x86_64/device/keyboard.cpp
@@ -2,7 +2,7 @@
 #include "com.h"
 #include "kernel.h"
 
-const char layout[128] =
+constexpr char layout[128] =
 {
 	0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	'9', '0', '-', '=', '\b', '\t',
 	'q', 'w', 'e', 'r',	't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
@@ -21,7 +21,7 @@ const char layout[128] =
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 };
 
-const char layout_upper[128] =
+constexpr char layout_upper[128] =
 {
 	0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	'(', ')', '_', '+', '\b', '\t',
 	'Q', 'W', 'E', 'R',	'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',
